2aee0356c520624d7272e6123025442a90db3c78
qgao21 <38676078+qgao21@users.noreply.github.com>
2023-10-06 20:38:04 +0800

    Add files via upload

---
+++ test_osl_framework.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=gpu_id python main.py\
+ --model_name corediff\
+ --run_name dose5_mayo2016\
+ --test_batch_size 1\
+ --test_dataset mayo_2016\
+ --test_id 9\
+ --context\
+ --only_adjust_two_step\
+ --dose 50\
+ --test_iter 150000\
+ --mode test_osl_framework\
+
---
+++ train.sh
@@ -0,0 +1,12 @@
+CUDA_VISIBLE_DEVICES=0 python main.py\
+ --model_name corediff\
+ --run_name dose5_mayo2016\
+ --batch_size 4\
+ --max_iter 150000\
+ --test_dataset mayo_2016\
+ --test_id 9\
+ --context\
+ --only_adjust_two_step\
+ --dose 5\
+ --save_freq 2500
+
