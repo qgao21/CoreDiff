9ac25dfa048edc4c516caca674738edde6afa8e8
qgao21 <38676078+qgao21@users.noreply.github.com>
2023-12-05 11:18:39 +0800

    Update test_osl_framework.sh

--- test_osl_framework.sh
+++ test_osl_framework.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=gpu_id python main.py\
  --model_name corediff\
- --run_name dose5_mayo2016\
+ --run_name dose5_mayo2016_sim\
  --test_batch_size 1\
- --test_dataset mayo_2016\
+ --test_dataset mayo_2016_sim\
  --test_id 9\
  --context\
  --only_adjust_two_step\
