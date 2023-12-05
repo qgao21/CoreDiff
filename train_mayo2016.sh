5dac392835fc98b1eba3763e78960373b5cdaad4
qgao21 <38676078+qgao21@users.noreply.github.com>
2023-12-05 11:10:36 +0800

    Update train_mayo2016.sh

--- train_mayo2016.sh
+++ train_mayo2016.sh
@@ -1,14 +1,12 @@
 CUDA_VISIBLE_DEVICES=0 python main.py\
  --model_name corediff\
- --run_name dose25_mayo_2016\
+ --run_name dose25_mayo_2016_ori\
  --batch_size 4\
  --max_iter 150000\
- --test_dataset mayo_2016\
+ --test_dataset mayo_2016_ori\
  --test_id 9\
  --context\
  --only_adjust_two_step\
  --dose 25\
  --save_freq 2500\
  --train_dataset mayo_2016\
- --wandb
-
