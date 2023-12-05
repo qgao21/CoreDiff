3f26c7c5485ad9393b1729e48aa9a04bc00f3319
qgao21 <38676078+qgao21@users.noreply.github.com>
2023-12-05 11:19:30 +0800

    Update train_mayo2016.sh

--- train_mayo2016.sh
+++ train_mayo2016.sh
@@ -1,9 +1,9 @@
-CUDA_VISIBLE_DEVICES=0 python main.py\
+CUDA_VISIBLE_DEVICES=gpu_id python main.py\
  --model_name corediff\
- --run_name dose25_mayo_2016_ori\
+ --run_name dose25_mayo_2016\
  --batch_size 4\
  --max_iter 150000\
- --test_dataset mayo_2016_ori\
+ --test_dataset mayo_2016\
  --test_id 9\
  --context\
  --only_adjust_two_step\
