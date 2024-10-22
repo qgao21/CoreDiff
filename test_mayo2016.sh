fd1242f5edda04867dc90c9a7ffb250bde2483c2
qgao21 <38676078+qgao21@users.noreply.github.com>
2024-10-22 11:09:50 +0800

    Add files via upload

---
+++ test_mayo2016.sh
@@ -0,0 +1,14 @@
+CUDA_VISIBLE_DEVICES=gpu_id python main.py\
+ --model_name corediff\
+ --run_name dose25_mayo_2016\
+ --batch_size 8\
+ --max_iter 150000\
+ --test_dataset mayo_2016\
+ --test_batch_size 1\
+ --test_id 9\
+ --context\
+ --only_adjust_two_step\
+ --dose 25\
+ --save_freq 2500\
+ --mode test\
+ --test_iter 150000
